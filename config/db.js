15dee7be4c3ef72abadcbdf0030d90c672af97de
Mustapa Zidan <zidan.pahrudin4@gmail.com>
2021-04-02 10:50:35 +0700

    adding user authentication and register

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongooURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      userCreateIndex: true,
+    });
+    console.log('mongoDB connected...');
+  } catch (err) {
+    console.log(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
